3fdd36ffdbd64d1dbe3287d83f40a6d580013cee
George Dernikos <geoder101@gmail.com>
2015-08-15 18:52:34 +0300

    add setup script

---
+++ docker/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ln -sf $PWD/rm-all.sh /usr/local/bin/docker-rm-all
+ln -sf $PWD/rm-build.sh /usr/local/bin/docker-rm-build
+ln -sf $PWD/rmi-build.sh /usr/local/bin/docker-rmi-build
+ln -sf $PWD/rmi-dangling.sh /usr/local/bin/docker-rmi-dangling
