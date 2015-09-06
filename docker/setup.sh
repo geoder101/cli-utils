7c1c69ca298706ed39ec59ed970e14d74dee54cb
George Dernikos <geoder101@gmail.com>
2015-09-06 23:15:49 +0300

    add 'pull-all.sh'

--- docker/setup.sh
+++ docker/setup.sh
@@ -4,3 +4,4 @@ ln -sf $PWD/rm-all.sh /usr/local/bin/docker-rm-all
 ln -sf $PWD/rm-build.sh /usr/local/bin/docker-rm-build
 ln -sf $PWD/rmi-build.sh /usr/local/bin/docker-rmi-build
 ln -sf $PWD/rmi-dangling.sh /usr/local/bin/docker-rmi-dangling
+ln -sf $PWD/pull-all.sh /usr/local/bin/docker-pull-all
